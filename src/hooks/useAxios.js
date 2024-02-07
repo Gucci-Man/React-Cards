d65c4c93c4566e20b0da125771449630208e5796
mrtomno <adel.ngo@yahoo.com>
2024-02-06 23:26:57 -0600

    update useAxios

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -6,9 +6,18 @@ import {v1 as uuid} from "uuid";
 // First element is an array of data obtained from previous AJAX requests
 // Second element is a function that will add a new object of data to the array
 const useAxios = (url) => {
+    let fullUrl = null;
     const [response, setResponse] = useState([]);
-    const addObj = async () => {
-        const res = await axios.get(url);
+    const addObj = async (obj) => {
+        // if obj is string, then create full url for pokemon
+        if (typeof(obj) === 'string') {       
+            fullUrl = url + obj
+        // else just use base url for cards  
+        } else {
+            fullUrl = url;
+        }
+        const res = await axios.get(fullUrl);
+        
         setResponse(response => [...response, { ...res.data, id: uuid() }]);
     };
     return [response, addObj]
